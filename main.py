7256e2d86392ab490e71544627fd8ddd6f1dabfb
guidoasbun <guido@asbun.io>
2022-05-22 10:13:05 -0700

    env test

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-print("Hello")
+import os
 
+# pip install python-dotenv
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env
 
+# env variables
 
+API_KEY = os.getenv("API_KEY")
+EMAIL_ADDR = os.getenv("EMAIL_ADDR")
+PASSWORD = os.getenv("PASSWORD")
+
+log_info = {
+    'api_key': API_KEY,
+    'email': EMAIL_ADDR,
+    'password': PASSWORD
+}
+
+print(log_info)
 
 
 
